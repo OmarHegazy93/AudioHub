e81b56f1e7726c33f8d9717ab597adc371656cf8
OmarHegazy93 <omarhegazy@mail.com>
2025-08-16 02:23:27 +0300

    Add project files

---
+++ AudioHub/AudioHub/AudioHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioHubApp.swift
+//  AudioHub
+//
+//  Created by Omar Hegazy on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct AudioHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioHub/AudioHubTests/AudioHubTests.swift
@@ -0,0 +1,16 @@
+//
+//  AudioHubTests.swift
+//  AudioHubTests
+//
+//  Created by Omar Hegazy on 16/08/2025.
+//
+
+import Testing
+
+struct AudioHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
